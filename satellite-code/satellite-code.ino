89699d53635b3ca518eedb2d553e365ca67207f2
Elena Justo <elenaj@protonmail.ch>
2023-10-20 18:33:14 +1100

    Initialised red LED and servo on sat source code

--- satellite-code/satellite-code.ino
+++ satellite-code/satellite-code.ino
@@ -5,12 +5,29 @@
 // Subject:     41099 Introduction to Mechatronics Engineering
 // Assessment:  Assessment 4
 
-void setup() {
-  // put your setup code here, to run once:
+// ************ Libraries ********************
+#include <Servo.h>
 
+// ************ Variables ********************
+
+int satLED = 4;         // Digital output pin for LED
+Servo satArm;           // Servo object. Pin assignment  in setup() loop. Pin = 9
+
+
+void setup() {
+
+  // Initialize the serial port:
+  Serial.begin(9600);
+
+  // Servo motor setup
+  satArm.attach(9);                             // Set pin 9 for servo control
+
+  satArm.write(0);
+
+  digitalWrite(satLED, HIGH); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
