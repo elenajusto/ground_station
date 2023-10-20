8cb62b636d072eceff90cf728365325c40b240e9
Elena Justo <elenaj@protonmail.ch>
2023-10-20 18:40:14 +1100

    Created function to move servo to 3 predefined positions. Program loop demonstrates servo movement in tandem with LED flash

--- satellite-code/satellite-code.ino
+++ satellite-code/satellite-code.ino
@@ -13,6 +13,21 @@
 int satLED = 4;         // Digital output pin for LED
 Servo satArm;           // Servo object. Pin assignment  in setup() loop. Pin = 9
 
+// ************ Functions ********************
+
+void moveSatArm(int position) {
+  switch(position) {
+    case 0:
+      satArm.write(0);
+      break;
+    case 90:
+      satArm.write(90);
+      break;
+    case 180:
+      satArm.write(180);
+      break;
+  }
+}
 
 void setup() {
 
@@ -22,12 +37,28 @@ void setup() {
   // Servo motor setup
   satArm.attach(9);                             // Set pin 9 for servo control
 
-  satArm.write(0);
-
   digitalWrite(satLED, HIGH); 
 }
 
 void loop() {
-  
 
+  digitalWrite(satLED, LOW);
+  Serial.println("Move arm to 0");
+  moveSatArm(0);
+  delay(1000);
+
+  digitalWrite(satLED, HIGH);
+  Serial.println("Move arm to 90");
+  moveSatArm(90);
+  delay(1000);
+
+  digitalWrite(satLED, LOW);
+  Serial.println("Move arm to 180");
+  moveSatArm(180);
+  delay(1000);
+
+  digitalWrite(satLED, HIGH);
+  Serial.println("Move arm to 90");
+  moveSatArm(90);
+  delay(1000);
 }
