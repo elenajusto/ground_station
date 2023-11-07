9ca4673729a667d11d3456823760b6e334cbcecb
Elena Justo <elenaj@protonmail.ch>
2023-11-07 12:52:30 +1100

    Initial sending/receive code

--- IR-code-receiver/IR-code-receiver.ino
+++ IR-code-receiver/IR-code-receiver.ino
@@ -1,17 +1,57 @@
 // ************ Description *****************
-// Program:     IR Communication Code - Receiver
+// Program:     IR Communication Code - Satellite
 // Name:        Elena Justo
 // SID:         24429298
 // Subject:     41099 Introduction to Mechatronics Engineering
 // Assessment:  Assessment 4
 
+// This program will listen for pings from the ground station and then return
+// an acknowledgement (ack) packet to confirm it has received a ping.
+
+// ************ Libraries ********************
+#include <IRremote.h>
+
+// ************ Variables ********************
+int IR_LED_PIN = 13;   
+IRsend irsend;             // Initialise the IRsend object
+
+String message = "ack";    // Message being sent
+
+int delayTime = 1000;      // Time to wait between each ping
+int messageDelay = 50;     // Time to wait between each character being sent
+
+int IR_RECEIVE_PIN = 11;         // The pin the IR receiver is connected to
+IRrecv irrecv(IR_RECEIVE_PIN);
+decode_results results;
+
+// ************ Functions ********************
+
+void sendAck(const String &message){
+  for (int i = 0; i < message.length(); i++) {  // Loop for each character in the message
+    irsend.sendNEC((uint32_t)message[i], 32);   // Send each character's ASCII value as a unique NEC code 
+    delay(messageDelay);                        // Wait between chars to avoid mix-up
+  }
+  delay(delayTime); 
+};
+
+void printReceive(){
+  if (irrecv.decode(&results)) {
+      if (results.decode_type == NEC) {
+        Serial.print((char)results.value);  // Print the received character as ASCII
+      }
+      irrecv.resume();                      // Prepare to receive the next value
+  }
+} 
+
+// ************ Program Loops ********************
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);                 // Initialise serial communications
+  pinMode(IR_LED_PIN, OUTPUT);        // Set the IR LED pin as output
+  irrecv.enableIRIn();                // Start the receiver
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("Listening");
+  printReceive();                     // Print received messages to serial monitor
 }
--- IR-code-transmitter/IR-code-transmitter.ino
+++ IR-code-transmitter/IR-code-transmitter.ino
@@ -1,17 +1,43 @@
 // ************ Description *****************
-// Program:     IR Communication Code - Transmitter
+// Program:     IR Communication Code - Ground Station
 // Name:        Elena Justo
 // SID:         24429298
 // Subject:     41099 Introduction to Mechatronics Engineering
 // Assessment:  Assessment 4
 
+// This program will send a ping to the satellite and output the acknowledgement message
+// that it is expected to receive from the satellite into the Serial monitor.
+
+// ************ Libraries ********************
+#include <IRremote.h>
+
+// ************ Variables ********************
+int IR_LED_PIN = 13;   
+IRsend irsend;             // Initialise the IRsend object
+
+String message = "ping";   // Message being sent
+
+int delayTime = 1000;      // Time to wait between each ping
+int messageDelay = 50;     // Time to wait between each character being sent
+
+// ************ Functions ********************
+
+void sendPing(const String &message){
+  for (int i = 0; i < message.length(); i++) {  // Loop for each character in the message
+    irsend.sendNEC((uint32_t)message[i], 32);   // Send each character's ASCII value as a unique NEC code 
+    delay(messageDelay);                        // Wait between chars to avoid mix-up
+  }
+  delay(delayTime); 
+};
+
+// ************ Program Loops ********************
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);                 // Initialise serial communications
+  pinMode(IR_LED_PIN, OUTPUT);        // Set the IR LED pin as output
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.print("Sending");
+  sendPing(message);                  // Sends ping to satellite
 }
